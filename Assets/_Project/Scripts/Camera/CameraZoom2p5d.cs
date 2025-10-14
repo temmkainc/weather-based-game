e1e5662e8787986aa8be41cef76baa644842ce46
Artem Hnap <temmkainc@gmail.com>
2025-10-14 16:14:15 +0200

    Feature: Sprite-Ready 3D Environment (#2)
    In this PR I Introduce ability to use 2D Sprites in our game perspective the way they react on light (shadows will be static for better optimization)

---
+++ Assets/_Project/Scripts/Camera/CameraZoom2p5d.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+namespace CameraSystem
+{
+    [RequireComponent(typeof(CameraFollow2P5D))]
+    [RequireComponent(typeof(Camera))]
+    public class CameraZoom2P5D : MonoBehaviour
+    {
+        [Header("Zoom Settings")]
+        public float zoomSpeed = 5f;
+        public float smoothTime = 0.15f;
+        public float minZoom = 6f;
+        public float maxZoom = 14f;
+
+        private Camera _cam;
+        private float _targetZoom;
+        private float _velocity;
+
+        private void Awake()
+        {
+            _cam = GetComponent<Camera>();
+            _targetZoom = _cam.orthographic ? _cam.orthographicSize : _cam.fieldOfView;
+        }
+
+        private void Update()
+        {
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+            if (Mathf.Abs(scroll) > 0.01f)
+            {
+                if (_cam.orthographic)
+                    _targetZoom -= scroll * zoomSpeed;
+                else
+                    _targetZoom -= scroll * zoomSpeed * 10f;
+
+                _targetZoom = Mathf.Clamp(_targetZoom, minZoom, maxZoom);
+            }
+
+            if (_cam.orthographic)
+            {
+                _cam.orthographicSize = Mathf.SmoothDamp(
+                    _cam.orthographicSize,
+                    _targetZoom,
+                    ref _velocity,
+                    smoothTime
+                );
+            }
+            else
+            {
+                _cam.fieldOfView = Mathf.SmoothDamp(
+                    _cam.fieldOfView,
+                    _targetZoom,
+                    ref _velocity,
+                    smoothTime
+                );
+            }
+        }
+    }
+}
