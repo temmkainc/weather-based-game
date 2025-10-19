cd14ab388713b23683dd0593774f4ff345d0cfb4
Artem Hnap <temmkainc@gmail.com>
2025-10-19 16:25:33 +0200

    Feature: Crop Growth Basics  (#4)
    In this PR I introduce CropData and CropBase classes that allows to create crops that differ on stages visuals & growth time. It also has water level that influence growth. We will add advanced parameters in future PR`s

---
+++ Assets/_Project/Scripts/Farming/CropBase.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+
+namespace Farming
+{
+    public class CropBase : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private CropData _data;
+        [SerializeField] private CropSceneContainer _sceneContainer;
+        [SerializeField] private SpriteRenderer _renderer;
+
+        [Header("Water / Drying")]
+        [SerializeField] private float _waterLevel;
+        [SerializeField] private float _waterDrainRate = 0.02f;
+
+        [Header("Growth")]
+        [SerializeField] private float _elapsedGrowthTime = 0f;
+        private int _currentStage = -1;
+        private bool _isDead = false;
+        private bool _isReady = false;
+
+        private void Start()
+        {
+            _waterLevel = 1f;
+            UpdateStage();
+        }
+
+        private void Setup(CropData data)
+        {
+            _data = data;
+            _waterLevel = 1f;
+            UpdateStage();
+        }
+
+        private void Update()
+        {
+            if (_isDead || _isReady) return;
+
+            _waterLevel -= _waterDrainRate * Time.deltaTime;
+            _waterLevel = Mathf.Clamp01(_waterLevel);
+
+            if (_waterLevel <= 0f)
+            {
+                Die();
+                return;
+            }
+
+            if (_waterLevel >= _data.WaterNeededToGrow)
+            {
+                _elapsedGrowthTime += Time.deltaTime;
+            }
+
+            if (_elapsedGrowthTime >= _data.GrowthStages[_currentStage].TimeToNextStage)
+            {
+                UpdateStage();
+            }
+
+            UpdateUI();
+        }
+
+        private void UpdateStage()
+        {
+            _currentStage++;
+
+            if (_currentStage >= _data.GrowthStages.Length)
+            {
+                Grow();
+                return;
+            }
+
+            _renderer.sprite = _data.GrowthStages[_currentStage].Sprite;
+            _elapsedGrowthTime = 0f;
+
+            if (_currentStage == _data.GrowthStages.Length - 1)
+            {
+                Grow();
+            }
+        }
+
+        private void UpdateUI()
+        {
+            _sceneContainer.WaterBar.fillAmount = _waterLevel;
+            _sceneContainer.GrowthBar.fillAmount = _elapsedGrowthTime / _data.GrowthStages[_currentStage].TimeToNextStage;
+        }
+
+        private void Grow()
+        {
+            _isReady = true;
+            _sceneContainer.CropUI.SetActive(false);
+
+            Debug.Log($"{_data.CropName} is Ready");
+        }
+
+        public void Water(float amount = 0.5f)
+        {
+            if (_isDead) return;
+            _waterLevel = Mathf.Clamp01(_waterLevel + amount);
+            _renderer.color = Color.white;
+        }
+
+        public void Harvest()
+        {
+            if (!_isReady) return;
+            Debug.Log($"Harvested {_data.CropName}");
+            Destroy(gameObject);
+        }
+
+        private void Die()
+        {
+            _isDead = true;
+            Debug.Log($"{_data.CropName} died.");
+        }
+    }
+}
